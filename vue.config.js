3fe7914af5a0f8980e7256bf54e2e56fa49f79b9
aondodawid <aondo1@gmail.com>
2026-03-22 16:38:00 +0100

    feat: suppress InjectManifest warning in dev mode
    - Filter console.warn to hide 'InjectManifest has been called multiple times' warning
    - Warning appears in watch mode but doesn't affect functionality
    - Only affects development environment, production unaffected

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,17 @@ const { InjectManifest } = require("workbox-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV === "production";
 
+// Filtruj warning o InjectManifest w dev mode
+if (!isProduction) {
+  const originalWarn = console.warn;
+  console.warn = function (...args) {
+    const message = args.join(" ");
+    if (!message.includes("InjectManifest has been called multiple times")) {
+      originalWarn.apply(console, args);
+    }
+  };
+}
+
 module.exports = {
   css: {
     extract: false,
